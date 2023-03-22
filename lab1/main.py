a6f112ce1fe5b3f7f4ea54cde8aadbbf05debc3c
shashkevichfrida <elfrida.shashkevich@gmail.com>
2023-03-22 14:24:43 +0300

    initial commit

---
+++ lab1/main.py
@@ -0,0 +1,11 @@
+from func_sin import *
+from func_exp import *
+
+def main():
+    exp_func = exp()
+    exp_func.draw()
+    sin_func = sin()
+    sin_func.draw()
+
+
+if __name__ == '__main__' : main()
